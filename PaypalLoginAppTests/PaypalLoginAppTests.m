dcd178ebf7b7f4ffd3f5a829eecfd30108913a13
Mermache <mermache101@gmail.com>
2014-08-04 12:50:58 -0700

    Initial Commit

---
+++ PaypalLoginAppTests/PaypalLoginAppTests.m
@@ -0,0 +1,34 @@
+//
+//  PaypalLoginAppTests.m
+//  PaypalLoginAppTests
+//
+//  Created by Mermache, Abdull(AWF) on 8/4/14.
+//  Copyright (c) 2014 Ebay.com. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PaypalLoginAppTests : XCTestCase
+
+@end
+
+@implementation PaypalLoginAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
